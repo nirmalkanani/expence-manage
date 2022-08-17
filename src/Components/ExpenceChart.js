e50cec944c5248daea6a6323e7b775db936e60e2
Maulik <just.maulik7@gmail.com>
2022-08-17 10:40:02 +0530

    Update

---
+++ src/Components/ExpenceChart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpenceChart = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ExpenceChart
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import ExpenceChart from './ExpenceChart'
 import ExpenseForm from './ExpenseForm'
 
 const Home = () => {
   return (
     <div>
       <ExpenseForm/>
+      <ExpenceChart/>
     </div>
   )
 }
