2bd3ca4ad00d8a033a324eff465236ad9f99cb7a
Maulik <just.maulik7@gmail.com>
2022-08-22 10:01:24 +0530

    Validation Compeleted

--- src/Components/ExpenseForm.js
+++ src/Components/ExpenseForm.js
@@ -13,17 +13,17 @@ const ExpenseForm = () => {
         description: "",
         amount: "",
         date: "",
-        month:""
+        month: ""
     }
 
     const [data, setData] = useState(INITIAL_STATE)
 
-    const [ dateDetail, setDateDetail ] = useState()
+    const [dateDetail, setDateDetail] = useState()
 
     const { description, amount, date } = data
 
     const handleChange = (e) => {
-        setData({ ...data ,[e.target.name]: e.target.value })
+        setData({ ...data, [e.target.name]: e.target.value })
     }
 
     const getData = useSelector((state) => state.dataReducer.expencesData)
@@ -37,15 +37,15 @@ const ExpenseForm = () => {
 
         const CheckData = description && amount && date
 
-        if(!CheckData){
+        if (!CheckData) {
             toast.error("Please Fill Data")
-        } else{
-            dispatch(SENDDATA({...data, date:DATE, month:parseInt(MONTH)}))
+        } else if (!/[0-9]/g.test(amount)) {
+            toast.error("Please Eter Only Numbers")
+        } else {
+            dispatch(SENDDATA({ ...data, date: DATE, month: parseInt(MONTH) }))
             setData(INITIAL_STATE)
         }
-
         e.preventDefault()
-        
     }
 
     return (
@@ -84,12 +84,13 @@ const ExpenseForm = () => {
                                         className='rounded'
                                         autoComplete='off'
                                     />
+                                    {/* { !/[0-9]/g.test(amount) ? "error" : "" } */}
                                 </div>
                                 <div className="input-gropu-section col-4 my-3">
                                     <input type="date" className='w-75 p-3 rounded' onChange={(e) => handleChange(e)} name="date" value={date} />
                                 </div>
                                 <div className="input-gropu-section col-5 my-3 text-end">
-                                    <input type="submit" className=' w-50 p-3 rounded btn btn-primary' value="Submit"/>
+                                    <input type="submit" className=' w-50 p-3 rounded btn btn-primary' value="Submit" />
                                 </div>
                             </div>
                         </form>
