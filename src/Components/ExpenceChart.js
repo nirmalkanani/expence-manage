ae189a25befa6d7e84c09fbac970fb2bc0c3b176
Maulik <just.maulik7@gmail.com>
2022-08-22 10:45:33 +0530

    Update ExpenceChart.js

--- src/Components/ExpenceChart.js
+++ src/Components/ExpenceChart.js
@@ -17,7 +17,7 @@ const ExpenceChart = () => {
 
     const [amount, setAmount] = useState()
 
-    const [ finalData, setFinalData ] = useState()
+    const [finalData, setFinalData] = useState()
 
     const getDate = (item) => {
 
@@ -26,7 +26,7 @@ const ExpenceChart = () => {
 
         item?.map((element, index) => {
             const finaldate = element.date
-            
+
             const Yy = moment(finaldate, "DD-MM-YYYY").format("YYYY")
             const Mm = moment(finaldate, "DD-MM-YYYY").format("MM")
             if (ALL_YEAR.includes(Yy)) {
@@ -40,21 +40,23 @@ const ExpenceChart = () => {
                 ALL_MONTH.push(Mm)
             }
         })
-        
+
         const MONTH = []
         const YEAR = []
 
         item?.map((element) => {
-            
+
             const MMMM = moment(element.date, "DD-MM-YYYY").format("MM")
-            if(!MONTH.includes(MMMM)){
+            if (!MONTH.includes(MMMM)) {
                 MONTH.push(MMMM)
             }
             const YYYY = moment(element.date, "DD-MM-YYYY").format("YYYY")
-            if(!YEAR.includes(YYYY)){
+            if (!YEAR.includes(YYYY)) {
                 YEAR.push(YYYY)
             }
         })
+
+
 
         setYears(ALL_YEAR.sort())
         setCheckMonths(MONTH.sort())
@@ -69,16 +71,16 @@ const ExpenceChart = () => {
 
     const handleChange = (e) => {
 
-        const FilterData = getData.filter((element, index) => moment(element.date, "DD-MM-YYYY").format("YYYY") === e.target.value)
-        
+        const FilterData = getData.filter((element) => moment(element.date, "DD-MM-YYYY").format("YYYY") === e.target.value)
+
         FilterData.filter((element) => {
-            
+
             const getMonth = moment(element.date, "DD-MM-YYYY").format("MM")
-            
-            if(checkMonths.includes(getMonth)){
+
+            if (checkMonths.includes(getMonth)) {
                 OUT_DATA.push(element)
             }
-            
+
             if (!ALL_MONTHS.includes(moment(element.date, "DD-MM-YYYY").format("MMMM"))) {
                 ALL_MONTHS.push(moment(element.date, "DD-MM-YYYY").format("MMMM"))
             }
@@ -97,89 +99,89 @@ const ExpenceChart = () => {
 
         const INITIAL_STATE = [
             {
-                month:1,
-                data:[],
-                amount:[],
-                total:0
+                month: 1,
+                data: [],
+                amount: [],
+                total: 0
             },
             {
-                month:2,
-                data:[],
-                amount:[],
-                total:0
+                month: 2,
+                data: [],
+                amount: [],
+                total: 0
             },
             {
-                month:3,
-                data:[],
-                amount:[],
-                total:0
+                month: 3,
+                data: [],
+                amount: [],
+                total: 0
             },
             {
-                month:4,
-                data:[],
-                amount:[],
-                total:0
+                month: 4,
+                data: [],
+                amount: [],
+                total: 0
             },
             {
-                month:5,
-                data:[],
-                amount:[],
-                total:0
+                month: 5,
+                data: [],
+                amount: [],
+                total: 0
             },
             {
-                month:6,
-                data:[],
-                amount:[],
-                total:0
+                month: 6,
+                data: [],
+                amount: [],
+                total: 0
             },
             {
-                month:7,
-                data:[],
-                amount:[],
-                total:0
+                month: 7,
+                data: [],
+                amount: [],
+                total: 0
             },
             {
-                month:8,
-                data:[],
-                amount:[],
-                total:0
+                month: 8,
+                data: [],
+                amount: [],
+                total: 0
             },
             {
-                month:9,
-                data:[],
-                amount:[],
-                total:0
+                month: 9,
+                data: [],
+                amount: [],
+                total: 0
             },
             {
-                month:10,
-                data:[],
-                amount:[],
-                total:0
+                month: 10,
+                data: [],
+                amount: [],
+                total: 0
             },
             {
-                month:11,
-                data:[],
-                amount:[],
-                total:0
+                month: 11,
+                data: [],
+                amount: [],
+                total: 0
             },
             {
-                month:12,
-                data:[],
-                amount:[],
-                total:0
+                month: 12,
+                data: [],
+                amount: [],
+                total: 0
             },
         ]
 
-        OUT_DATA?.map((data,index) => {
+        OUT_DATA?.map((data) => {
             INITIAL_STATE.map((element) => {
-                if(element.month === data.month){
+                if (element.month === data.month) {
                     element.data.push(data)
                 }
             })
         })
 
-        INITIAL_STATE.map((data, index) => {
-            const A = data.data.map((element, index) => {
+        INITIAL_STATE.map((data) => {
+            const A = data.data.map((element) => {
                 data.amount.push(eval(element.amount))
 
                 for (let i = 0; i < data.amount.length; i++) {
@@ -217,30 +219,27 @@ const ExpenceChart = () => {
             <div className="row text-end">
                 <div className="col-12 my-5">
                     <select name="GetYear" id="years" className='px-3 py-2 text-dark' onChange={(e) => handleChange(e)}>
-                        <option value="Select Year">Select Year</option>
+                        <option value={new Date().getFullYear()}>Select Year</option>
                         {
-                            years?.map((element, index) => <option value={element} key={index} name="GetYear" >{element}</option>)
+                               years?.map((element, index) => <option value={element} key={index} name="GetYear">{element}</option>)
                         }
                     </select>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-12">
-                    <div className="bar-chart">
-                        <div className="row">
-                            {
-                                amount?.map((element, index) =>
-                                    <div className="col-1" key={index}>
-                                        <div className="progress " style={{ height: "500px", position: "relative" }} >
-                                            <div className="real-time-progress bg-dark rounded" style={{ height: `${element}%`, width: "100%", position: "absolute", bottom: "0" }}></div>
-                                        </div>
-                                    </div>
-                                )
-                            }
-                        </div>
-                    </div>
+            <div className="bar-chart">
+                <div className="row">
+                    {
+                        years ? amount?.map((element, index) =>
+                            <div className="col-1" key={index}>
+                                <div className="progress " style={{ height: "500px", position: "relative" }} >
+                                    <div className="real-time-progress bg-dark rounded" style={{ height: `${element}%`, width: "100%", position: "absolute", bottom: "0" }}></div>
+                                </div>
+                            </div>
+                        ) :""
+                    }
                 </div>
             </div>
+            
             {/* Months Value Pass From Here Via using Props */}
             <ExpenceByMonths year={years} month={months} alldata={dataByMonths} />
         </div>
