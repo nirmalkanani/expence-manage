ca450fa0de6d61da71b1593b4094e11b7a11b743
Maulik <just.maulik7@gmail.com>
2022-08-23 10:27:41 +0530

    Update ExpenceByMonths.js

--- src/Components/ExpenceByMonths.js
+++ src/Components/ExpenceByMonths.js
@@ -5,13 +5,13 @@ import { DELETE, EDITDATA } from '../Redux/Actions/Action'
 import { useDispatch } from 'react-redux'
 
 const ExpenceByMonths = (props) => {
-    
-    const [ alldata, setAllData ] = useState()  
 
-    const [ objKeys, setObjKeys ] = useState()
+    const [alldata, setAllData] = useState()
 
-    const [ objValues, setObjValues ] = useState()
-    
+    const [objKeys, setObjKeys] = useState()
+
+    const [objValues, setObjValues] = useState()
+
     const GETDATA = async () => {
         const response = await axios.get('https://expense-tracker-fbcff-default-rtdb.asia-southeast1.firebasedatabase.app/expense.json')
         const OB_KEYS = Object.keys(response.data)
@@ -19,7 +19,7 @@ const ExpenceByMonths = (props) => {
         const OB_VALUES = Object.values(response.data)
         setObjValues(OB_VALUES)
 
-        const ADD = OB_KEYS.map((element, index) => 
+        const ADD = OB_KEYS.map((element, index) =>
             OB_VALUES[index].key = OB_KEYS[index]
         )
 
@@ -30,22 +30,26 @@ const ExpenceByMonths = (props) => {
 
     useEffect(() => {
         GETDATA()
-    },[])
+    }, [])
 
-    const handleChange = (e) =>{   
+    const handleChange = (e) => {
 
-        const FilterData = props.alldata.filter((element,index) => moment(element.date,"DD-MM-YYYY").format("MMMM") === e.target.value)
+        const FilterData = props.alldata.filter((element, index) => moment(element.date, "DD-MM-YYYY").format("MMMM") === e.target.value)
         setAllData(FilterData)
     }
 
     const handleDelete = (key) => {
         console.log(key)
-        
+
         dispatch(DELETE(key))
     }
-    
-    const handleEdit = (key)=> {
+
+    const handleEdit = (key) => {
+
         console.log(key)
+
+        const A = props.alldata.filter((element, index) => element.key === key)
+        console.log(A)
         // dispatch(EDITDATA(key))
     }
 
@@ -54,7 +58,7 @@ const ExpenceByMonths = (props) => {
             <div className='row text-end'>
                 <div className="col-12 my-5">
                     <select name="month" id="month" className='px-3 py-2 text-dark' onChange={(e) => handleChange(e)}>
-                    <option value="Select Month" >Select Month</option>
+                        <option value="Select Month" >Select Month</option>
                         {
                             props.month?.map((element, index) => <option value={element} key={index} name="GetMonth" >{element}</option>)
                         }
@@ -63,18 +67,24 @@ const ExpenceByMonths = (props) => {
             </div>
             <div className="row border border-2 border-dark p-4">
                 {
-                    alldata?.map((element,index) => 
+                    alldata?.map((element, index) =>
                         <div className="col-12 my-4" key={index}>
                             <div className="row border border-2 border-dark rounded p-3 align-items-center">
                                 <div className="col-md-3">
                                     <div className="date-box text-center border border-1 border-dark rounded p-3">
                                         <h2 className='text-dark'>{moment(element.date, "DD-MM-YYYY").format("DD")}</h2>
-                                        <h4>{ moment(element.date, "DD-MM-YYYY").format("MMMM") }</h4>
+                                        <h4>{moment(element.date, "DD-MM-YYYY").format("MMMM")}</h4>
                                     </div>
                                 </div>
                                 <div className="col-md-9">
-                                    <div className="amount-box border border-2 border-dark rounded mb-3 p-2"><h3>{element.amount}</h3></div>
-                                    <div className="decription-box border border-2 border-dark rounded  p-2"><h4>{element.description}</h4></div>
+                                    <form>
+                                        <div>
+                                            <input type='text' className="amount-box border border-2 border-dark rounded mb-3 p-2" value={element.amount} />
+                                        </div>
+                                        <div>
+                                            <input type='text' className="decription-box border border-2 border-dark rounded  p-2" value={element.description} />
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                             <div className="row">
